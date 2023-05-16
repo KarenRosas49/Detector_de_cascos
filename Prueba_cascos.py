8cd619e4f61641bcc850b403f5acd7a312d4e1ce
Karen Rosas <rosas.karen0926@gmail.com>
2023-05-16 11:59:18 -0600

    Comentarios en codigos

--- Prueba_cascos.py
+++ Prueba_cascos.py
@@ -1,24 +1,24 @@
+#Código para probar el modelo entrenado
+
 import cv2
 import os
 import mediapipe as mp
 #import urllib.request #para abrir y leer URL
 
-url = 'http://192.168.100.26'
+url = 'http://192.168.100.26' #Se coloca el url de la esp32cam
 
-mp_face_detection = mp.solutions.face_detection
+mp_face_detection = mp.solutions.face_detection #Se manda a llamar la solución de la librería mediapipe para la detección de rostros
 
-LABELS = ["Con_casco", "Sin_casco"]
+LABELS = ["Con_casco", "Sin_casco"] #Se coloca el nombre de los dos grupos entrenados con el programa train.py
 
 # Leer el modelo
-#face_helmet = cv2.face.LBPHFaceRecognizer_create()
-# Métodos para entrenar el reconocedor
 face_helmet = cv2.face.EigenFaceRecognizer_create()
-#face_recognizer = cv2.face.FisherFaceRecognizer_create()
-#face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 face_helmet.read("modelo_cascos1.xml")
 
+#Lectura del video
+
 #cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap = cv2.VideoCapture(url + ":81/stream")
+cap = cv2.VideoCapture(url + ":81/stream") #Se lee el url de la esp32cam
 
 with mp_face_detection.FaceDetection(
      min_detection_confidence=0.5) as face_detection:
@@ -26,12 +26,15 @@ with mp_face_detection.FaceDetection(
      while True:
           ret, frame = cap.read()
           if ret == False: break
-          frame = cv2.flip(frame, 1)
+          frame = cv2.flip(frame, 1) #Se invierte la imagen
 
+
+          #Se cambia la imagen a RGB
           height, width, _ = frame.shape
-          frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-          results = face_detection.process(frame_rgb)
+          frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+          results = face_detection.process(frame_rgb) #Se observan los resultados
 
+          #Se dibuja el cuadrado que detecta el rostro
           if results.detections is not None:
                for detection in results.detections:
                     xmin = int(detection.location_data.relative_bounding_box.xmin * width)
@@ -40,15 +43,16 @@ with mp_face_detection.FaceDetection(
                     h = int(detection.location_data.relative_bounding_box.height * height)
                     if xmin < 0 and ymin < 0:
                          continue
-                    #cv2.rectangle(frame, (xmin, ymin), (xmin + w, ymin + h), (0, 255, 0), 5)
-
+                    
+                    #Se extrae la información de los rostros y se aplica escala de grises
                     face_image = frame[ymin : ymin + h, xmin : xmin + w]
                     face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
-                    face_image = cv2.resize(face_image, (72, 72), interpolation=cv2.INTER_CUBIC)
+                    face_image = cv2.resize(face_image, (72, 72), interpolation=cv2.INTER_CUBIC) #Se redimenciona el tama;o de imagen a 72x72 pixeles
                     
+                    #Se aplica el modelo entrenado
                     result = face_helmet.predict(face_image)
-                    #cv2.putText(frame, "{}".format(result), (xmin, ymin - 5), 1, 1.3, (210, 124, 176), 1, cv2.LINE_AA)
-
+                    
+                    #Dibuja el rectángulo y le coloca una etiqueta
                     if result[1] < 5700:
                          color = (0, 255, 0) if LABELS[result[0]] == "Con_casco" else (0, 0, 255)
                          cv2.putText(frame, "{}".format(LABELS[result[0]]), (xmin, ymin - 15), 2, 1, color, 1, cv2.LINE_AA)
--- Train.py
+++ Train.py
@@ -1,34 +1,37 @@
+#Código para el entrenamiento del reconocedor
+
 #Se importan las librerías
 import cv2
 import os 
 import numpy as np
 
 dataPath = "/home/karen/Documents/GitHub/Detector_de_cascos/Dataset" #Especificar la ruta donde se encuentra almacenado el dataset
-dir_list = os.listdir(dataPath)
-print("Lista archivos:", dir_list)
+dir_list = os.listdir(dataPath) #Se listan las carpetas que se encuentran en la carpeta especificada
+print("Lista archivos:", dir_list) #Se muestra en la consola el nombre de las carpetas dentro del directorio donde se encuentran los datasets
  
-labels = []
-facesData = []
-label = 0
+labels = [] #Arrays que se utilizará para almacenar el valor correspondiente a "con_casco" y un valor correspondiente a "sin_casco"
+facesData = [] #Array que se utilizará para almacenar la etiqueta con_"casco" y la etiqueta "sin_casco"
+label = 0 #Contador que se iniciará en cero
 
+#Lectura de cada una de las imágenes almacenadas en los datasets
+     #El primer "for" hace lectura de cada una de las carpetas con los datasets seleccionados
+     #El segundi "for" lee cada una de las imágenes guardadas en cada carpeta
 for name_dir in dir_list:
      dir_path = dataPath + "/" + name_dir
      
      for file_name in os.listdir(dir_path):
           image_path = dir_path + "/" + file_name
           print(image_path)
-          image = cv2.imread(image_path, 0)
-          #cv2.imshow("Image", image)
-          #cv2.waitKey(10)
-
-          facesData.append(image)
-          labels.append(label)
+          image = cv2.imread(image_path, 0) #Se lee la imagen y con el 0 se especifica que es escala de grises
+          
+          facesData.append(image) #Se almacena cada imagen en escala de grises
+          labels.append(label) #Se almacena el valor correspondiente a cada rostro, es decir, en cada imagen se coloca si correspode a "Con_casco" o si corresponde a "Sin_casco"
      label += 1
 
-print("Etiqueta 0: ", np.count_nonzero(np.array(labels) == 0))
-print("Etiqueta 1: ", np.count_nonzero(np.array(labels) == 1))
+print("Etiqueta 0: ", np.count_nonzero(np.array(labels) == 0)) #Se cuentan cuántos ceros hay
+print("Etiqueta 1: ", np.count_nonzero(np.array(labels) == 1)) #Se cuentan cuántos unos hay
 
-# LBPH FaceRecognizer
+# Eigen FaceRecognizer
 face_mask = cv2.face.EigenFaceRecognizer_create()
 
 # Entrenamiento
